1df687f4dfee0d31c07e2fff53ac39db970909f9
feralvam <feralvam@gmail.com>
2019-03-18 10:04:47 +0000

    removed function for only splitting sentences.

--- easse/utils/preprocessing.py
+++ easse/utils/preprocessing.py
@@ -1,10 +1,5 @@
 import sacrebleu
 import sacremoses
-import os
-from stanfordnlp.server import CoreNLPClient
-
-
-os.environ['CORENLP_HOME'] = "/tools/stanford-corenlp-full-2018-10-05"
 
 
 def normalize(sentence, lowercase: bool = True, tokenizer: str = '13a', return_str: bool = True):
@@ -24,17 +19,3 @@ def normalize(sentence, lowercase: bool = True, tokenizer: str = '13a', return_s
         normalized_sent = normalized_sent.split()
 
     return normalized_sent
-
-
-def split_into_sentences(text, normalized=True, return_str: bool = False):
-    if not normalized:
-        text = normalize(text)
-
-    with CoreNLPClient(annotators=['tokenize','ssplit'], properties={'tokenize.whitespace': True}) as client:
-        ann = client.annotate(text)
-        if return_str:
-            sentences = [' '.join([t.word for t in s.token]) for s in ann.sentence]
-        else:
-            sentences = [[t.word for t in s.token] for s in ann.sentence]
-
-    return sentences
