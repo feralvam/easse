d45dbebbf60f4b51596a7dd490433be162ff02c1
Louis MARTIN <louisrtm@gmail.com>
2019-06-18 08:16:46 -0700

    Add FKGL to easse

---
+++ easse/fkgl.py
@@ -0,0 +1,37 @@
+from easse.utils.text import to_sentences, count_words, count_syllables_in_sentence
+
+
+class FKGLScorer:
+    '''https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests'''
+
+    def __init__(self):
+        self.nb_words = 0
+        self.nb_syllables = 0
+        self.nb_sentences = 0
+
+    def add(self, text):
+        for sentence in to_sentences(text):
+            self.nb_words += count_words(sentence)
+            self.nb_syllables += count_syllables_in_sentence(sentence)
+            self.nb_sentences += 1
+
+    def score(self):
+        # Flesch-Kincaid grade level
+        print(self.nb_sentences, self.nb_words, self.nb_syllables)
+        if self.nb_sentences == 0 or self.nb_words == 0:
+            return 0
+        return max(0, 0.39 * (self.nb_words / self.nb_sentences) + 11.8 * (self.nb_syllables / self.nb_words) - 15.59)
+
+
+def get_fkgl(filepath):
+    scorer = FKGLScorer()
+    with open(filepath, 'r') as f:
+        for line in f:
+            scorer.add(line.strip())
+    return scorer.score()
+
+
+def get_sentence_fkgl(sentence):
+    scorer = FKGLScorer()
+    scorer.add(sentence)
+    return scorer.score()
