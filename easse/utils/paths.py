88846da95f07ab132699e4b9a20e149bf78a4a2e
Louis MARTIN <louisrtm@gmail.com>
2019-07-04 01:26:37 -0700

    Refactor config.json path

--- easse/utils/paths.py
+++ easse/utils/paths.py
@@ -4,4 +4,5 @@ from pathlib import Path
 REPO_DIR = Path(__file__).resolve().parent.parent.parent
 TOOLS_DIR = REPO_DIR / 'tools'
 DATA_DIR = REPO_DIR / 'data'
+CONFIG_PATH = REPO_DIR / 'easse/config.json'
 STANFORD_CORENLP_PATH = TOOLS_DIR / 'stanford-corenlp-full-2018-10-05'
