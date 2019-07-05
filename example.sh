11a23d864ee66cf5e3beef9fbe56efed35d0da72
Louis MARTIN <louisrtm@gmail.com>
2019-07-05 05:49:50 -0700

    Make paths absolute in example.sh

--- example.sh
+++ example.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-easse evaluate -t turk -m 'bleu,sari,fkgl' -q < data/system_outputs/turk/lower/DMASS-DCSS.tok.low
-easse report -t turk -m 'bleu,sari,fkgl' < data/system_outputs/turk/lower/DMASS-DCSS.tok.low
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+easse evaluate -t turk -m 'bleu,sari,fkgl' -q < "$SCRIPT_DIR/data/system_outputs/turk/lower/DMASS-DCSS.tok.low"
+easse report -t turk -m 'bleu,sari,fkgl' < "$SCRIPT_DIR/data/system_outputs/turk/lower/DMASS-DCSS.tok.low"
