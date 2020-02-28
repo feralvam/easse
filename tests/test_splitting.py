4ce1c81d36723d86b80de0ee1d106d2e1e0098de
feralvam <feralvam@gmail.com>
2020-02-28 14:18:02 +0000

    added tests for split-and-rephrase metrics.

---
+++ tests/test_splitting.py
@@ -0,0 +1,16 @@
+import pytest
+
+from easse.splitting import corpus_macro_avg_sent_bleu
+from easse.utils.helpers import read_split_lines, collapse_split_sentences
+from easse.utils.constants import DATA_DIR
+
+
+def test_compute_macro_avg_sent_bleu():
+    # REFERENCE scores
+    sys_sents = read_split_lines(DATA_DIR / "test_sets/wikisplit/wikisplit.test.untok.split")
+    refs_sents = [read_split_lines(DATA_DIR / "test_sets/wikisplit/wikisplit.test.untok.split")]
+
+    collapsed_sys_sents, collapsed_refs_sents = collapse_split_sentences(sys_sents, refs_sents)
+
+    sbleu = corpus_macro_avg_sent_bleu(collapsed_sys_sents, collapsed_refs_sents)
+    assert sbleu == pytest.approx(100.0)
