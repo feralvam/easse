83484d6a926b298474bf2bda61b280c5424101be
feralvam <feralvam@gmail.com>
2020-02-28 14:22:05 +0000

    special case for reading wikisplit.

--- easse/utils/resources.py
+++ easse/utils/resources.py
@@ -6,8 +6,8 @@ import time
 from urllib.request import urlretrieve
 import zipfile
 
-from easse.utils.constants import DATA_DIR, STANFORD_CORENLP_DIR, UCCA_DIR, UCCA_PARSER_PATH
-from easse.utils.helpers import get_temp_filepath, read_lines
+from easse.utils.constants import STANFORD_CORENLP_DIR, UCCA_DIR, UCCA_PARSER_PATH, TEST_SETS_PATHS, SYSTEM_OUTPUTS_DIRS_MAP
+from easse.utils.helpers import get_temp_filepath, read_lines, read_split_lines
 
 
 def reporthook(count, block_size, total_size):
@@ -53,50 +53,37 @@ def download_stanford_corenlp():
     unzip(temp_filepath, STANFORD_CORENLP_DIR.parent)
 
 
+def update_ucca_path():
+    # HACK: Change vocab_path from relative to absolute path
+    json_path = str(UCCA_PARSER_PATH) + '.nlp.json'
+    with open(json_path, 'r') as f:
+        config_json = json.load(f)
+    config_json['vocab'] = str(UCCA_DIR / 'vocab/en_core_web_lg.csv')
+    with open(json_path, 'w') as f:
+            json.dump(config_json, f)
+
+
 def download_ucca_model():
     url = 'https://github.com/huji-nlp/tupa/releases/download/v1.3.10/ucca-bilstm-1.3.10.tar.gz'
     temp_filepath = get_temp_filepath()
     download(url, temp_filepath)
     UCCA_DIR.mkdir(parents=True, exist_ok=True)
     untar(temp_filepath, UCCA_DIR)
-    # HACK: Change vocab_path from relative to absolute path
-    json_path = str(UCCA_PARSER_PATH) + '.nlp.json'
-    with open(json_path, 'r') as f:
-        config_json = json.load(f)
-    config_json['vocab'] = str(UCCA_DIR / config_json['vocab'])
-    with open(json_path, 'w') as f:
-            json.dump(config_json, f)
+    update_ucca_path()
 
 
-def get_turk_orig_sents(phase):
-    assert phase in ['valid', 'test']
-    if phase == 'valid':
-        phase = 'tune'
-    return read_lines(DATA_DIR / f'test_sets/turk/{phase}.8turkers.tok.norm')
+def get_orig_sents(test_set):
+    return read_lines(TEST_SETS_PATHS[(test_set, 'orig')])
 
 
-def get_turk_refs_sents(phase):
-    assert phase in ['valid', 'test']
-    if phase == 'valid':
-        phase = 'tune'
-    return [read_lines(DATA_DIR / f'test_sets/turk/{phase}.8turkers.tok.turk.{i}')
-            for i in range(8)]
+def get_refs_sents(test_set):
+    if test_set in ['wikisplit', 'wikisplit_valid']:
+        refs_sents = [read_split_lines(ref_sents_path) for ref_sents_path in TEST_SETS_PATHS[(test_set, 'refs')]]
+    else:
+        refs_sents = [read_lines(ref_sents_path) for ref_sents_path in TEST_SETS_PATHS[(test_set, 'refs')]]
+
+    return refs_sents
 
 
-def get_pwkp_orig_sents(phase):
-    assert phase in ['valid', 'test']
-    return read_lines(DATA_DIR / f'test_sets/pwkp/pwkp.{phase}.src')
-
-
-def get_pwkp_refs_sents(phase):
-    assert phase in ['valid', 'test']
-    return [read_lines(DATA_DIR / f'test_sets/pwkp/pwkp.{phase}.dst')]
-
-
-def get_hsplit_orig_sents():
-    return get_turk_orig_sents(phase='test')
-
-
-def get_hsplit_refs_sents():
-    return [read_lines(DATA_DIR / f'test_sets/hsplit/hsplit.tok.{i+1}')
-            for i in range(4)]
+def get_system_outputs_dir(test_set):
+    return SYSTEM_OUTPUTS_DIRS_MAP[test_set]
