1a6f29e8843df5cfa335fa5615bce15635f4b9d2
feralvam <feralvam@gmail.com>
2020-03-09 16:11:02 +0000

    added paths for turkcorpus_test_legacy and turkcorpus_valid_legacy

--- easse/utils/constants.py
+++ easse/utils/constants.py
@@ -12,14 +12,18 @@ UCCA_PARSER_PATH = UCCA_DIR / 'models/ucca-bilstm'
 TEST_SETS_PATHS = {
         ('turkcorpus_test', 'orig'): DATA_DIR / f'test_sets/turkcorpus/test.truecase.detok.orig',
         ('turkcorpus_test', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/test.truecase.detok.simp.{i}' for i in range(8)],
+        ('turkcorpus_test_legacy', 'orig'): DATA_DIR / f'test_sets/turkcorpus/legacy/test.8turkers.tok.norm',
+        ('turkcorpus_test_legacy', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/legacy/test.8turkers.tok.turk.{i}' for i in range(8)],
         ('turkcorpus_valid', 'orig'): DATA_DIR / f'test_sets/turkcorpus/tune.truecase.detok.orig',
         ('turkcorpus_valid', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/tune.truecase.detok.simp.{i}' for i in range(8)],
+        ('turkcorpus_valid_legacy', 'orig'): DATA_DIR / f'test_sets/turkcorpus/legacy/tune.8turkers.tok.norm',
+        ('turkcorpus_valid_legacy', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/legacy/tune.8turkers.tok.turk.{i}' for i in range(8)],
         ('pwkp_test', 'orig'): DATA_DIR / f'test_sets/pwkp/pwkp.test.orig',
         ('pwkp_test', 'refs'): [DATA_DIR / f'test_sets/pwkp/pwkp.test.simp'],
         ('pwkp_valid', 'orig'): DATA_DIR / f'test_sets/pwkp/pwkp.valid.orig',
         ('pwkp_valid', 'refs'): [DATA_DIR / f'test_sets/pwkp/pwkp.valid.simp'],
-        ('hsplit_test', 'orig'): DATA_DIR / f'test_sets/hsplit/hsplit.tok.src',
-        ('hsplit_test', 'refs'): [DATA_DIR / f'test_sets/hsplit/hsplit.tok.{i+1}' for i in range(4)],
+        ('hsplit_test', 'orig'): DATA_DIR / f'test_sets/hsplit/hsplit.recase.detok.src',
+        ('hsplit_test', 'refs'): [DATA_DIR / f'test_sets/hsplit/hsplit.recase.detok.{i+1}' for i in range(4)],
         ('wikisplit_test', 'orig'): DATA_DIR / f'test_sets/wikisplit/wikisplit.test.untok.orig',
         ('wikisplit_test', 'refs'): [DATA_DIR / f'test_sets/wikisplit/wikisplit.test.untok.split'],
         ('wikisplit_valid', 'orig'): DATA_DIR / f'test_sets/wikisplit/wikisplit.valid.untok.orig',
@@ -41,9 +45,9 @@ SYSTEM_OUTPUTS_DIRS_MAP = {
 }
 
 # Constants
-VALID_TEST_SETS = ['turkcorpus_test', 'turkcorpus_valid', 'pwkp_test', 'pwkp_valid', 'hsplit_test',
-                   'wikisplit_test', 'wikisplit_valid', 'asset_test', 'asset_valid', 'googlecomp',
-                   'googlecomp_valid', 'custom']
+VALID_TEST_SETS = ['turkcorpus_test', 'turkcorpus_valid','turkcorpus_test_legacy', 'turkcorpus_valid_legacy',
+                   'pwkp_test', 'pwkp_valid', 'hsplit_test', 'wikisplit_test', 'wikisplit_valid', 'asset_test',
+                   'asset_valid', 'googlecomp', 'googlecomp_valid', 'custom']
 VALID_METRICS = ['bleu', 'sari', 'samsa', 'fkgl', 'sbleu', 'length_stats', 'f1_token', 'comp_ratio']
 DEFAULT_METRICS = [m for m in VALID_METRICS if m not in ['samsa', 'sbleu', 'length_stats', 'f1_token', 'comp_ratio']]
 # HACK: Only simplification-specific metrics, but SAMSA which is too long to compute
