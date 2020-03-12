2ef45d377f7b5a1d32656c716126937456880a76
feralvam <feralvam@gmail.com>
2020-03-12 15:27:00 +0000

    test now uses turkcorpus_test_legacy

--- tests/test_fkgl.py
+++ tests/test_fkgl.py
@@ -4,7 +4,6 @@ from easse.fkgl import corpus_fkgl
 from easse.utils.resources import get_orig_sents, get_refs_sents
 
 
-@pytest.mark.skip(reason='TODO: Test broken, need to investigate')
 def test_corpus_fkgl():
-    assert corpus_fkgl(get_orig_sents('turkcorpus_test')) == pytest.approx(9.9, abs=1e-1)
-    assert corpus_fkgl(get_refs_sents('turkcorpus_test')[0]) == pytest.approx(8.2, abs=1e-1)
+    assert corpus_fkgl(get_orig_sents('turkcorpus_test_legacy')) == pytest.approx(9.9, abs=1e-1)
+    assert corpus_fkgl(get_refs_sents('turkcorpus_test_legacy')[0]) == pytest.approx(8.2, abs=1e-1)
