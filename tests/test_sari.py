8f2277c56cab838d8432c43a6a7acf3e869e6229
feralvam <feralvam@gmail.com>
2020-03-12 15:30:37 +0000

    removed test_corpus_sari

--- tests/test_sari.py
+++ tests/test_sari.py
@@ -26,37 +26,6 @@ def test_sentence_sari():
     assert sari_score == pytest.approx(50.71608864657479)
 
 
-@pytest.mark.skip(reason='TODO: Test probably broken, need to investigate')
-def test_corpus_sari():
-    orig_sents = get_orig_sents('turkcorpus_test')
-    refs_sents = get_refs_sents('turkcorpus_test')
-    system_outputs_dir = get_system_outputs_dir('turkcorpus_test')
-
-    hyp_sents = read_lines(system_outputs_dir / "lower/Dress-Ls.tok.low")
-    sari_score = sari.corpus_sari(orig_sents, hyp_sents, refs_sents)
-    assert sari_score == pytest.approx(37.266058818588216)
-
-    hyp_sents = read_lines(system_outputs_dir / "lower/Dress.tok.low")
-    sari_score = sari.corpus_sari(orig_sents, hyp_sents, refs_sents)
-    assert sari_score == pytest.approx(37.08210095744638)
-
-    hyp_sents = read_lines(system_outputs_dir / "lower/EncDecA.tok.low")
-    sari_score = sari.corpus_sari(orig_sents, hyp_sents, refs_sents)
-    assert sari_score == pytest.approx(35.65754396121206)
-
-    hyp_sents = read_lines(system_outputs_dir / "lower/Hybrid.tok.low")
-    sari_score = sari.corpus_sari(orig_sents, hyp_sents, refs_sents)
-    assert sari_score == pytest.approx(31.39665078989411)
-
-    hyp_sents = read_lines(system_outputs_dir / "lower/PBMT-R.tok.low")
-    sari_score = sari.corpus_sari(orig_sents, hyp_sents, refs_sents)
-    assert sari_score == pytest.approx(38.558843050332037)
-
-    hyp_sents = read_lines(system_outputs_dir / "lower/SBMT-SARI.tok.low")
-    sari_score = sari.corpus_sari(orig_sents, hyp_sents, refs_sents)
-    assert sari_score == pytest.approx(39.964857928109127)
-
-
 def test_corpus_sari_legacy():
     orig_sents = get_orig_sents('turkcorpus_test_legacy')
     refs_sents = get_refs_sents('turkcorpus_test_legacy')
