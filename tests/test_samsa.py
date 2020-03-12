caf8a6bed65cbee5cc3c14fa5ab8c0591c0cde67
feralvam <feralvam@gmail.com>
2020-03-12 14:24:26 +0000

    fixed test_get_scenes

--- tests/test_samsa.py
+++ tests/test_samsa.py
@@ -1,74 +1,78 @@
+import pytest
+from easse.utils.ucca_utils import get_scenes_text, ucca_parse_texts
+
 from easse import samsa
 
 
-# def test_get_scenes():
-#     text = 'You are waiting for a train , this train will take you far away .'
-#     expected_scenes = [
-#         ['You', 'are', 'waiting', 'for', 'a', 'train'],
-#         ['this', 'train', 'will', 'take', 'you', 'far', 'away']
-#     ]
-#     scenes = get_scenes(ucca_parse_text(text))
-#     assert scenes == expected_scenes
+def test_get_scenes():
+    text = 'You are waiting for a train , this train will take you far away .'
+    expected_scenes = [
+        ['You', 'are', 'waiting', 'for', 'a', 'train'],
+        ['this', 'train', 'will', 'take', 'you', 'far', 'away']
+    ]
+    scenes = get_scenes_text(ucca_parse_texts([text])[0])
+    assert scenes == expected_scenes
 
 
+@pytest.mark.skip(reason='TODO: Test probably broken, need to investigate')
 def test_samsa_score_sentence():
-    # orig_sentence = "You are waiting for a train , this train will take you far away ."
-    # sys_output = "You are waiting for a train . A train that will take you far away ."
-    # samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=True)
-    # assert samsa_score == pytest.approx(1.0)
+    orig_sentence = "You are waiting for a train , this train will take you far away ."
+    sys_output = "You are waiting for a train . A train that will take you far away ."
+    samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=True)
+    assert samsa_score == pytest.approx(1.0)
 
-    # orig_sentence = ("The river is indeed an ever-present part of the city's decor , and the official entrance "
-    #                  "to Lisbon is a broad marble stair mounting from the water to the vast , "
-    #                  "arcaded Commerce Square ( Praca do Comercio) .")
-    # sys_output = ("The river is indeed an ever-present part of the city's decor , and the entrance to Lisbon "
-    #               "is a broad marble stair mounting from the water to the covering , arcaded Commerce "
-    #               "Square ( Praca do Comercio) .")
-    # samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=True)
-    # assert samsa_score == pytest.approx(0.25)
+    orig_sentence = ("The river is indeed an ever-present part of the city's decor , and the official entrance "
+                     "to Lisbon is a broad marble stair mounting from the water to the vast , "
+                     "arcaded Commerce Square ( Praca do Comercio) .")
+    sys_output = ("The river is indeed an ever-present part of the city's decor , and the entrance to Lisbon "
+                  "is a broad marble stair mounting from the water to the covering , arcaded Commerce "
+                  "Square ( Praca do Comercio) .")
+    samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=True)
+    assert samsa_score == pytest.approx(0.25)
 
-    # orig_sentence = ("The second largest city of Russia and one of the world's major cities , "
-    #                  "St . Petersburg has played a vital role in Russian history .")
-    # sys_output = ("The second largest city of Russia and one of the world's major cities , "
-    #               "St Petersburg , and has played a vital role in Russian history .")
-    # samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=True)
-    # assert samsa_score == pytest.approx(0.833333333)
+    orig_sentence = ("The second largest city of Russia and one of the world's major cities , "
+                     "St . Petersburg has played a vital role in Russian history .")
+    sys_output = ("The second largest city of Russia and one of the world's major cities , "
+                  "St Petersburg , and has played a vital role in Russian history .")
+    samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=True)
+    assert samsa_score == pytest.approx(0.833333333)
 
-    # orig_sentence = ("The incident followed the killing in August of five Egyptian security guards by "
-    #                  "Israeli soldiers pursuing militants who had ambushed and killed eight Israelis "
-    #                  "along the Israeli-Egyptian border.")
-    # sys_output = ("The incident followed the killing in August by Israeli soldiers. "
-    #               "Israeli soldiers pursued militants. "
-    #               "Militants had ambushed and killed eight Israelis along the Israeli-Egyptian border.")
-    # samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=True)
-    # assert samsa_score == pytest.approx(0.71875)
+    orig_sentence = ("The incident followed the killing in August of five Egyptian security guards by "
+                     "Israeli soldiers pursuing militants who had ambushed and killed eight Israelis "
+                     "along the Israeli-Egyptian border.")
+    sys_output = ("The incident followed the killing in August by Israeli soldiers. "
+                  "Israeli soldiers pursued militants. "
+                  "Militants had ambushed and killed eight Israelis along the Israeli-Egyptian border.")
+    samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=True)
+    assert samsa_score == pytest.approx(0.71875)
 
-    # orig_sentence = ("The injured man was able to drive his car to Cloverhill Prison where he got help. "
-    #                  "He is being treated at Tallaght  Hospital but his injuries are not thought to be life-threatening.")
-    # sys_output = "The injured man drive his car to Cloverhill Prison he got help."
-    # samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output])
-    # assert samsa_score == pytest.approx(0.2222222222222222)
+    orig_sentence = ("The injured man was able to drive his car to Cloverhill Prison where he got help. "
+                     "He is being treated at Tallaght  Hospital but his injuries are not thought to be life-threatening.")
+    sys_output = "The injured man drive his car to Cloverhill Prison he got help."
+    samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output])
+    assert samsa_score == pytest.approx(0.2222222222222222)
 
-    orig_sentence = ("""waugh writes that charles had been " in search of love in those days " when he first met sebastian , 
-    finding " that low door in the wall ... which opened on an enclosed and enchanted garden " , 
-    a metaphor that informs the work on a number of levels .""")
-    sys_output = ("""however writes that charles had been '' in search of love in those days '' when he met sebastian , 
-    finding '' that low door in the wall ... which opened on an enclosed and magical garden '' , 
-    a metaphor that tells the work on a number of levels .
-    """)
+    orig_sentence = ("for example , king bhumibol was born on monday , "
+                     "so on his birthday throughout thailand will be decorated with yellow color .")
+    sys_output = ("for example , king bhumibol was born on monday , "
+                  "so on his birthday throughout thailand will be decorated with yellow color .")
     samsa_score = samsa.corpus_samsa([orig_sentence], [sys_output], lowercase=False)
+    # TODO: Missing assert?
     print(samsa_score)
 
 
-# def test_samsa_score_qats():
-#     # read the sentence pairs from QATS test set
-#     with open("data/qats/test.shared-task.tsv", newline='') as qats_file:
-#         reader = csv.reader(qats_file, delimiter='\t')
-#         next(reader, None)  # skip the header
-#         orig_sentences = []
-#         sys_outputs = []
-#         for row in reader:
-#             orig_sentences.append(row[0])
-#             sys_outputs.append(row[1])
-#
-#     samsa_score = samsa.corpus_samsa(orig_sentences, sys_outputs, lowercase=True)
-#     print(samsa_score)
+@pytest.mark.skip(reason='TODO: Test probably broken, need to investigate')
+def test_samsa_score_qats():
+    # read the sentence pairs from QATS test set
+    with open("resources/qats/test.shared-task.tsv", newline='') as qats_file:
+        reader = csv.reader(qats_file, delimiter='\t')
+        next(reader, None)  # skip the header
+        orig_sentences = []
+        sys_outputs = []
+        for row in reader:
+            orig_sentences.append(row[0])
+            sys_outputs.append(row[1])
+
+    samsa_score = samsa.corpus_samsa(orig_sentences, sys_outputs, lowercase=True)
+    # TODO: Missing assert?
+    print(samsa_score)
