c6476da76e206285932f130763b72e7ad8d0dbae
Louis MARTIN <louisrtm@gmail.com>
2020-08-31 08:39:39 -0700

    NIT

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
-with open('requirements.txt', 'r') as fh:
-    requirements = fh.read().strip().split('\n')
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().strip().split('\n')
 
 
 setup(
@@ -13,15 +13,15 @@ setup(
     version='0.2.1',
     description='Easier Automatic Sentence Simplification Evaluation',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Text Processing :: Linguistic",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     author='Fernando Alva-Manchego <feralvam@gmail.com>, Louis Martin <louismartincs@gmail.com>',
     url='https://github.com/feralvam/easse',
@@ -29,7 +29,7 @@ setup(
     include_package_data=True,
     test_suite='tests',
     entry_points={'console_scripts': [
-        "easse = easse.cli:cli"
+        'easse = easse.cli:cli'
     ]},
     install_requires=requirements,
 )
