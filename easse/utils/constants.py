16ec5ed2aa0d362da8d7d549255a64d7ac1245c1
Louis Martin <louismartin@fb.com>
2020-09-26 13:01:37 +0200

    Minor fix

--- easse/utils/constants.py
+++ easse/utils/constants.py
@@ -10,12 +10,16 @@ STANFORD_CORENLP_DIR = TOOLS_DIR / 'stanford-corenlp-full-2018-10-05'
 UCCA_DIR = TOOLS_DIR / 'ucca-bilstm-1.3.10'
 UCCA_PARSER_PATH = UCCA_DIR / 'models/ucca-bilstm'
 TEST_SETS_PATHS = {
+        ('asset_test', 'orig'): DATA_DIR / f'test_sets/asset/asset.test.orig',
+        ('asset_test', 'refs'): [DATA_DIR / f'test_sets/asset/asset.test.simp.{i}' for i in range(10)],
+        ('asset_valid', 'orig'): DATA_DIR / f'test_sets/asset/asset.valid.orig',
+        ('asset_valid', 'refs'): [DATA_DIR / f'test_sets/asset/asset.valid.simp.{i}' for i in range(10)],
         ('turkcorpus_test', 'orig'): DATA_DIR / f'test_sets/turkcorpus/test.truecase.detok.orig',
         ('turkcorpus_test', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/test.truecase.detok.simp.{i}' for i in range(8)],
-        ('turkcorpus_test_legacy', 'orig'): DATA_DIR / f'test_sets/turkcorpus/legacy/test.8turkers.tok.norm',
-        ('turkcorpus_test_legacy', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/legacy/test.8turkers.tok.turk.{i}' for i in range(8)],
         ('turkcorpus_valid', 'orig'): DATA_DIR / f'test_sets/turkcorpus/tune.truecase.detok.orig',
         ('turkcorpus_valid', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/tune.truecase.detok.simp.{i}' for i in range(8)],
+        ('turkcorpus_test_legacy', 'orig'): DATA_DIR / f'test_sets/turkcorpus/legacy/test.8turkers.tok.norm',
+        ('turkcorpus_test_legacy', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/legacy/test.8turkers.tok.turk.{i}' for i in range(8)],
         ('turkcorpus_valid_legacy', 'orig'): DATA_DIR / f'test_sets/turkcorpus/legacy/tune.8turkers.tok.norm',
         ('turkcorpus_valid_legacy', 'refs'): [DATA_DIR / f'test_sets/turkcorpus/legacy/tune.8turkers.tok.turk.{i}' for i in range(8)],
         ('pwkp_test', 'orig'): DATA_DIR / f'test_sets/pwkp/pwkp.test.orig',
@@ -28,10 +32,6 @@ TEST_SETS_PATHS = {
         ('wikisplit_test', 'refs'): [DATA_DIR / f'test_sets/wikisplit/wikisplit.test.untok.split'],
         ('wikisplit_valid', 'orig'): DATA_DIR / f'test_sets/wikisplit/wikisplit.valid.untok.orig',
         ('wikisplit_valid', 'refs'): [DATA_DIR / f'test_sets/wikisplit/wikisplit.valid.untok.split'],
-        ('asset_test', 'orig'): DATA_DIR / f'test_sets/asset/asset.test.orig',
-        ('asset_test', 'refs'): [DATA_DIR / f'test_sets/asset/asset.test.simp.{i}' for i in range(10)],
-        ('asset_valid', 'orig'): DATA_DIR / f'test_sets/asset/asset.valid.orig',
-        ('asset_valid', 'refs'): [DATA_DIR / f'test_sets/asset/asset.valid.simp.{i}' for i in range(10)],
         ('googlecomp_test', 'orig'): DATA_DIR / f'test_sets/googlecomp/googlecomp.test.orig',
         ('googlecomp_test', 'refs'): [DATA_DIR / f'test_sets/googlecomp/googlecomp.test.comp'],
         ('googlecomp_valid', 'orig'): DATA_DIR / f'test_sets/googlecomp/googlecomp.valid.orig',
@@ -48,8 +48,6 @@ SYSTEM_OUTPUTS_DIRS_MAP = {
 }
 
 # Constants
-VALID_TEST_SETS = ['turkcorpus_test', 'turkcorpus_valid','turkcorpus_test_legacy', 'turkcorpus_valid_legacy',
-                   'pwkp_test', 'pwkp_valid', 'hsplit_test', 'wikisplit_test', 'wikisplit_valid', 'asset_test',
-                   'asset_valid', 'googlecomp_test', 'googlecomp_valid', 'qats_test', 'custom']
+VALID_TEST_SETS = list(set([test_set for test_set, language in TEST_SETS_PATHS.keys()])) + ['custom']
 VALID_METRICS = ['bleu', 'sari', 'samsa', 'fkgl', 'sent_bleu', 'f1_token', 'sari_legacy']
 DEFAULT_METRICS = ['bleu', 'sari', 'fkgl']
