b25a16cba7bc57aafd5237c2768039df161fb7e6
feralvam <feralvam@gmail.com>
2020-10-11 16:07:35 +0100

    changed to version 0.2.3

--- setup.py
+++ setup.py
@@ -1,35 +1,33 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().strip().split('\n')
+with open("requirements.txt", "r") as f:
+    requirements = f.read().strip().split("\n")
 
 
 setup(
-    name='easse',
-    version='0.2.2',
-    description='Easier Automatic Sentence Simplification Evaluation',
+    name="easse",
+    version="0.2.3",
+    description="Easier Automatic Sentence Simplification Evaluation",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Text Processing :: Linguistic',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    author='Fernando Alva-Manchego <feralvam@gmail.com>, Louis Martin <louismartincs@gmail.com>',
-    url='https://github.com/feralvam/easse',
-    packages=find_packages(exclude=['tests']),
+    author="Fernando Alva-Manchego <feralvam@gmail.com>, Louis Martin <louismartincs@gmail.com>",
+    url="https://github.com/feralvam/easse",
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
-    test_suite='tests',
-    entry_points={'console_scripts': [
-        'easse = easse.cli:cli'
-    ]},
+    test_suite="tests",
+    entry_points={"console_scripts": ["easse = easse.cli:cli"]},
     install_requires=requirements,
 )
