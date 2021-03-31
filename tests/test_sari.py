7d8cdd93be655e1f2dd929efed7696e45ea5ed0d
louismartin <louismartincs@gmail.com>
2021-03-31 01:41:02 -0700

    Add test_corpus_sari with ACCESS score test

--- tests/test_sari.py
+++ tests/test_sari.py
@@ -53,3 +53,12 @@ def test_corpus_sari_legacy():
     hyp_sents = read_lines(system_outputs_dir / "tok.low/SBMT-SARI.tok.low")
     sari_score = sari.corpus_sari(orig_sents, hyp_sents, refs_sents, legacy=True)
     assert sari_score == pytest.approx(39.964857928109127)
+
+
+def test_corpus_sari():
+    orig_sents = get_orig_sents('turkcorpus_test')
+    refs_sents = get_refs_sents('turkcorpus_test')
+    system_outputs_dir = get_system_outputs_dir('turkcorpus_test')
+    hyp_sents = read_lines(system_outputs_dir / "ACCESS")
+    sari_score = sari.corpus_sari(orig_sents, hyp_sents, refs_sents)
+    assert sari_score == pytest.approx(41.381013)  # Scores from MUSS https://arxiv.org/abs/2005.00352
